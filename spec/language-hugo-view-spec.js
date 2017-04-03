c1db1c3a1cfac221d045088d6d902fa58d89122f
Matt Stratton <matt.stratton@gmail.com>
2017-04-03 10:13:52 -0500

    Initial commit

---
+++ spec/language-hugo-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageHugoView from '../lib/language-hugo-view';
+
+describe('LanguageHugoView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
