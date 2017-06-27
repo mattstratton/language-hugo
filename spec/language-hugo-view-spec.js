774111a1d290ef259fe2696c3dc57444beff5b17
Matt Stratton <matt.stratton@gmail.com>
2017-06-27 08:15:14 -0500

    Add simple spec

--- spec/language-hugo-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageHugoView from '../lib/language-hugo-view';
-
-describe('LanguageHugoView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
